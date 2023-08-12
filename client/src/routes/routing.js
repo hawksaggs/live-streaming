60547b2d15b5c0abf32d739b5924bc6cbf023355
Ayush Mittal <ayush.mittal@outlook.com>
2023-08-12 08:44:24 +0530

    live page

--- client/src/routes/routing.js
+++ client/src/routes/routing.js
@@ -1,52 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/homePage/homePage";
-import DashboardHome from '../pages/dashboardHome/dashboardHome';
-import HostPage from '../pages/hostPage/hostPage';
+import DashboardHome from "../pages/dashboardHome/dashboardHome";
+import HostPage from "../pages/hostPage/hostPage";
 import DashboardEvent from "../pages/dashboardEvent/dashboardEvent";
 import SignUpPage from "../pages/signup/signupPage";
 import LoginPage from "../pages/login/loginPage";
+import LivePage from "../pages/livePage/livePage";
 
 function Routing() {
-    return (
-
-        <div className="App">
-            <Routes>
-                <Route
-                    path="/"
-                    element={<HomePage />}
-                />
-                <Route
-                    path="/dashboard/home"
-                    element={<DashboardHome />}
-                />
-                <Route
-                    path="/dashboard/home/control-room"
-                    element={<DashboardHome controlRoom={true} />}
-                />
-                <Route
-                    path="/host"
-                    element={<HostPage />}
-                />
-                <Route
-                    path="/guests"
-                    element={<HostPage guestsPage />}
-                />
-                <Route
-                    path="/dashboard/event"
-                    element={<DashboardEvent />}
-                />
-                <Route
-                    path="/register"
-                    element={<SignUpPage />}
-                />
-                <Route
-                    path="/login"
-                    element={<LoginPage />}
-                />
-            </Routes>
-        </div>
-
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard/home" element={<DashboardHome />} />
+        <Route
+          path="/dashboard/home/control-room"
+          element={<DashboardHome controlRoom={true} />}
+        />
+        <Route path="/host" element={<HostPage />} />
+        <Route path="/guests" element={<HostPage guestsPage />} />
+        <Route path="/dashboard/event" element={<DashboardEvent />} />
+        <Route path="/register" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/live" element={<LivePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Routing;
