542ffc706c776be75110de8448a95db13daac14b
anindya <freelancerdevani@gmail.com>
2023-08-28 22:53:54 +0530

    protected route code modified

--- client/src/routes/RouteGuard.js
+++ client/src/routes/RouteGuard.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Route, Navigate, } from "react-router-dom";
 
 const RouteGuard = ({ children }) => {
   const token = localStorage.getItem("token");
-  console.log("token: ", token);
   console.log(!token);
-  if (!token) {
-    <Navigate to={"/login"} />;
+  if (!token || token === "null" || token === "undefined") {
+    return <Navigate to={"/login"} />;
   }
-
   return children;
 };
 
--- client/src/routes/routing.js
+++ client/src/routes/routing.js
@@ -1,38 +1,80 @@
-import { Route, Routes, create, Router } from "react-router-dom";
+// import { Route, Routes, create, Router } from "react-router-dom";
+// import HomePage from "../pages/homePage/homePage";
+// import DashboardHome from "../pages/dashboardHome/dashboardHome";
+// // import HostPage from "../pages/hostPage/hostPage";
+// import HostPage from "../pages/hostPage/hostPage";
+// import DashboardEvent from "../pages/dashboardEvent/dashboardEvent";
+// import SignUpPage from "../pages/signup/signupPage";
+// import LoginPage from "../pages/login/loginPage";
+// import LivePage from "../pages/livePage/livePage";
+
+// //history
+// import { history } from "../helpers";
+// import RouteGuard from "./RouteGuard";
+
+// function Routing() {
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/dashboard/home" element={<DashboardHome />} />
+//         <Route
+//           path="/dashboard/home/control-room"
+//           element={
+//             <RouteGuard>
+//               <DashboardHome controlRoom={true} />
+//             </RouteGuard>
+//           }
+//         />
+//         <Route path="/host" element={<HostPage />} />
+//         <Route path="/guests/:eventId" element={<HostPage guestsPage />} />
+//         <Route path="/dashboard/event" element={<DashboardEvent />} />
+//         <Route path="/register" element={<SignUpPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/live" element={<LivePage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default Routing;
+
+import { Route, Routes, Router } from "react-router-dom";
 import HomePage from "../pages/homePage/homePage";
 import DashboardHome from "../pages/dashboardHome/dashboardHome";
-// import HostPage from "../pages/hostPage/hostPage";
 import HostPage from "../pages/hostPage/hostPage";
 import DashboardEvent from "../pages/dashboardEvent/dashboardEvent";
 import SignUpPage from "../pages/signup/signupPage";
 import LoginPage from "../pages/login/loginPage";
 import LivePage from "../pages/livePage/livePage";
 
-//history
-import { history } from "../helpers";
+// history
+// import { createBrowserHistory } from "history";
 import RouteGuard from "./RouteGuard";
+
+// const history = createBrowserHistory();
 
 function Routing() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard/home" element={<DashboardHome />} />
-        <Route
-          path="/dashboard/home/control-room"
-          element={
-            <RouteGuard>
-              <DashboardHome controlRoom={true} />
-            </RouteGuard>
-          }
-        />
-        <Route path="/host" element={<HostPage />} />
-        <Route path="/guests/:eventId" element={<HostPage guestsPage />} />
-        <Route path="/dashboard/event" element={<DashboardEvent />} />
-        <Route path="/register" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/live" element={<LivePage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard/home" element={<DashboardHome />} />
+          <Route
+            path="/dashboard/home/control-room"
+            element={
+              <RouteGuard>
+                <DashboardHome controlRoom={true} />
+              </RouteGuard>
+            }
+          />
+          <Route path="/host" element={<HostPage />} />
+          <Route path="/guests/:eventId" element={<HostPage guestsPage />} />
+          <Route path="/dashboard/event" element={<DashboardEvent />} />
+          <Route path="/register" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/live" element={<LivePage />} />
+        </Routes>
     </div>
   );
 }
